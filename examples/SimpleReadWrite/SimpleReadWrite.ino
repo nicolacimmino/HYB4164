eb7a34517cdcb1a690e3f4a4caa0443887cee7e8
Nicola Cimmino <nicola.cimmino@gmail.com>
2023-06-01 17:27:17 +0200

    Wrap in namespace

--- examples/SimpleReadWrite/SimpleReadWrite.ino
+++ examples/SimpleReadWrite/SimpleReadWrite.ino
@@ -2,7 +2,7 @@
 
 void setup()
 {
-    dram4164_begin();
+    dram4164::begin();
     Serial.begin(9600);
 }
 
@@ -10,48 +10,48 @@ void loop()
 {
     // Write and read a single bit.
     // Max address is 65535.
-    dram4164_writeBit(0, 1);
-    Serial.println(dram4164_readBit(0));
+    dram4164::writeBit(0, 1);
+    Serial.println(dram4164::readBit(0));
 
     // Write and read a single nibble.
     // Max address is 16383.
-    dram4164_writeNibble(0, 0xA);
-    Serial.println(dram4164_readNibble(0), 16);
+    dram4164::writeNibble(0, 0xA);
+    Serial.println(dram4164::readNibble(0), 16);
 
     // Write and read a single byte.
     // Max address is 8191.
-    dram4164_writeByte(0, 0xB);
-    Serial.println(dram4164_readByte(0), 16);
+    dram4164::writeByte(0, 0xB);
+    Serial.println(dram4164::readByte(0), 16);
 
     // Write and read a single 16-bit word.
     // Max address is 4095.
-    dram4164_writeWord(0, (uint16_t)0x5A5A);
-    Serial.println(dram4164_readWord<uint16_t>(0), 16);
+    dram4164::writeWord(0, (uint16_t)0x5A5A);
+    Serial.println(dram4164::readWord<uint16_t>(0), 16);
 
     // Write and read a single 32-bit word.
     // Max address is 2047.
-    dram4164_writeWord(0, (uint32_t)0xA0125A5A);
-    Serial.println(dram4164_readWord<uint32_t>(0), 16);
+    dram4164::writeWord(0, (uint32_t)0xA0125A5A);
+    Serial.println(dram4164::readWord<uint32_t>(0), 16);
 
     // Write a single 32-bit word and read it back
     //  as bytes. Note that the numbers are written
     //  as little-endians, so the LSB will be in 0
     //  and the MSB in 3.
-    dram4164_writeWord(0, (uint32_t)0xA0125A5A);
-    Serial.println(dram4164_readByte(0), 16);
-    Serial.println(dram4164_readByte(1), 16);
-    Serial.println(dram4164_readByte(2), 16);
-    Serial.println(dram4164_readByte(3), 16);
+    dram4164::writeWord(0, (uint32_t)0xA0125A5A);
+    Serial.println(dram4164::readByte(0), 16);
+    Serial.println(dram4164::readByte(1), 16);
+    Serial.println(dram4164::readByte(2), 16);
+    Serial.println(dram4164::readByte(3), 16);
 
     // Read and write a 256 bits block. This is passed to
     //  and from the functions as as 32-byte array.
     // Max address is 256.
     uint8_t buffer[32];
     memset(buffer, 0xA5, 32);
-    dram4164_writeBlock(256, 0, buffer);
+    dram4164::writeBlock(256, 0, buffer);
 
     memset(buffer, 0xA5, 32);
-    dram4164_readBlock(256, 0, buffer);
+    dram4164::readBlock(256, 0, buffer);
 
     for (uint8_t ix = 0; ix < 32; ix++)
     {
--- src/DRAM4164Driver.cpp
+++ src/DRAM4164Driver.cpp
@@ -1,180 +1,184 @@
 #include "DRAM4164Driver.h"
 
-void dram4164_begin() {
-    pinMode(PIN_VCC, OUTPUT);
-    pinMode(PIN_GND, OUTPUT);
-
-    digitalWrite(PIN_VCC, HIGH);
-    digitalWrite(PIN_GND, LOW);
-
-    pinMode(PIN_RAS_N, OUTPUT);
-    pinMode(PIN_CAS_N, OUTPUT);
-    pinMode(PIN_WE_N, OUTPUT);
-    pinMode(PIN_A0, OUTPUT);
-    pinMode(PIN_A1, OUTPUT);
-    pinMode(PIN_A2, OUTPUT);
-    pinMode(PIN_A3, OUTPUT);
-    pinMode(PIN_A4, OUTPUT);
-    pinMode(PIN_A5, OUTPUT);
-    pinMode(PIN_A6, OUTPUT);
-    pinMode(PIN_A7, OUTPUT);
-    pinMode(PIN_DI, OUTPUT);
-    pinMode(PIN_DO, INPUT);
-
-    digitalWrite(PIN_RAS_N, HIGH);
-    digitalWrite(PIN_CAS_N, HIGH);
-
-    delay(100);
-
-    for (uint16_t ix = 0; ix < 256; ix++)
+namespace dram4164
+{
+    void begin()
     {
-        dram4164_writeBit(ix, 1);
+        pinMode(PIN_VCC, OUTPUT);
+        pinMode(PIN_GND, OUTPUT);
+
+        digitalWrite(PIN_VCC, HIGH);
+        digitalWrite(PIN_GND, LOW);
+
+        pinMode(PIN_RAS_N, OUTPUT);
+        pinMode(PIN_CAS_N, OUTPUT);
+        pinMode(PIN_WE_N, OUTPUT);
+        pinMode(PIN_A0, OUTPUT);
+        pinMode(PIN_A1, OUTPUT);
+        pinMode(PIN_A2, OUTPUT);
+        pinMode(PIN_A3, OUTPUT);
+        pinMode(PIN_A4, OUTPUT);
+        pinMode(PIN_A5, OUTPUT);
+        pinMode(PIN_A6, OUTPUT);
+        pinMode(PIN_A7, OUTPUT);
+        pinMode(PIN_DI, OUTPUT);
+        pinMode(PIN_DO, INPUT);
+
+        digitalWrite(PIN_RAS_N, HIGH);
+        digitalWrite(PIN_CAS_N, HIGH);
+
+        delay(100);
+
+        for (uint16_t ix = 0; ix < 256; ix++)
+        {
+            writeBit(ix, 1);
+        }
     }
-}
 
-void dram4164_setAddress(uint8_t address)
-{
-    digitalWriteFast(PIN_A0, address & 0x1);
-    digitalWriteFast(PIN_A1, (address >> 1) & 0x1);
-    digitalWriteFast(PIN_A2, (address >> 2) & 0x1);
-    digitalWriteFast(PIN_A3, (address >> 3) & 0x1);
-    digitalWriteFast(PIN_A4, (address >> 4) & 0x1);
-    digitalWriteFast(PIN_A5, (address >> 5) & 0x1);
-    digitalWriteFast(PIN_A6, (address >> 6) & 0x1);
-    digitalWriteFast(PIN_A7, (address >> 7) & 0x1);
-}
-
-void dram4164_writeBit(uint16_t address, bool value)
-{
-    dram4164_setAddress((address >> 8) & 0xFF);
-    digitalWriteFast(PIN_RAS_N, LOW);
-
-    digitalWriteFast(PIN_WE_N, LOW);
-
-    digitalWriteFast(PIN_DI, value ? HIGH : LOW);
-
-    dram4164_setAddress(address & 0xFF);
-    digitalWriteFast(PIN_CAS_N, LOW);
-
-    digitalWriteFast(PIN_WE_N, HIGH);
-    digitalWriteFast(PIN_CAS_N, HIGH);
-    digitalWriteFast(PIN_RAS_N, HIGH);
-}
-
-bool dram4164_readBit(uint16_t address)
-{
-    dram4164_setAddress((address >> 8) & 0xFF);
-    digitalWriteFast(PIN_RAS_N, LOW);
-
-    dram4164_setAddress(address & 0xFF);
-    digitalWriteFast(PIN_CAS_N, LOW);
-
-    delayMicroseconds(2);
-    bool value = (digitalReadFast(PIN_DO) == HIGH);
-
-    digitalWriteFast(PIN_CAS_N, HIGH);
-    digitalWriteFast(PIN_RAS_N, HIGH);
-
-    return value;
-}
-
-void dram4164_writeNibble(uint16_t address, uint8_t value)
-{
-    dram4164_setAddress((address >> 6) & 0xFF);
-    digitalWriteFast(PIN_RAS_N, LOW);
-
-    for (uint8_t ix = 0; ix < 4; ix++)
+    void setAddress(uint8_t address)
     {
+        digitalWriteFast(PIN_A0, address & 0x1);
+        digitalWriteFast(PIN_A1, (address >> 1) & 0x1);
+        digitalWriteFast(PIN_A2, (address >> 2) & 0x1);
+        digitalWriteFast(PIN_A3, (address >> 3) & 0x1);
+        digitalWriteFast(PIN_A4, (address >> 4) & 0x1);
+        digitalWriteFast(PIN_A5, (address >> 5) & 0x1);
+        digitalWriteFast(PIN_A6, (address >> 6) & 0x1);
+        digitalWriteFast(PIN_A7, (address >> 7) & 0x1);
+    }
+
+    void writeBit(uint16_t address, bool value)
+    {
+        setAddress((address >> 8) & 0xFF);
+        digitalWriteFast(PIN_RAS_N, LOW);
+
         digitalWriteFast(PIN_WE_N, LOW);
 
-        digitalWriteFast(PIN_DI, (value >> ix) & 0x1);
+        digitalWriteFast(PIN_DI, value ? HIGH : LOW);
 
-        dram4164_setAddress((address << 2) | ix);
+        setAddress(address & 0xFF);
         digitalWriteFast(PIN_CAS_N, LOW);
 
         digitalWriteFast(PIN_WE_N, HIGH);
         digitalWriteFast(PIN_CAS_N, HIGH);
+        digitalWriteFast(PIN_RAS_N, HIGH);
     }
 
-    digitalWriteFast(PIN_RAS_N, HIGH);
-}
-
-uint8_t dram4164_readNibble(uint16_t address)
-{
-    dram4164_setAddress((address >> 6) & 0xFF);
-    digitalWriteFast(PIN_RAS_N, LOW);
-
-    uint8_t value = 0;
-    for (uint8_t ix = 0; ix < 4; ix++)
+    bool readBit(uint16_t address)
     {
-        dram4164_setAddress((address << 2) | ix);
+        setAddress((address >> 8) & 0xFF);
+        digitalWriteFast(PIN_RAS_N, LOW);
+
+        setAddress(address & 0xFF);
         digitalWriteFast(PIN_CAS_N, LOW);
 
         delayMicroseconds(2);
-        value = value | ((digitalReadFast(PIN_DO) ? 1 : 0) << ix);
+        bool value = (digitalReadFast(PIN_DO) == HIGH);
 
         digitalWriteFast(PIN_CAS_N, HIGH);
+        digitalWriteFast(PIN_RAS_N, HIGH);
+
+        return value;
     }
 
-    digitalWriteFast(PIN_RAS_N, HIGH);
-
-    return value;
-}
-
-void dram4164_writeBlock(uint16_t blockSize, uint16_t address, uint8_t *value)
-{
-    uint8_t bits = 0;
-    uint16_t tmpBlockSize = blockSize;
-    while (tmpBlockSize = tmpBlockSize >> 1)
+    void writeNibble(uint16_t address, uint8_t value)
     {
-        bits++;
+        setAddress((address >> 6) & 0xFF);
+        digitalWriteFast(PIN_RAS_N, LOW);
+
+        for (uint8_t ix = 0; ix < 4; ix++)
+        {
+            digitalWriteFast(PIN_WE_N, LOW);
+
+            digitalWriteFast(PIN_DI, (value >> ix) & 0x1);
+
+            setAddress((address << 2) | ix);
+            digitalWriteFast(PIN_CAS_N, LOW);
+
+            digitalWriteFast(PIN_WE_N, HIGH);
+            digitalWriteFast(PIN_CAS_N, HIGH);
+        }
+
+        digitalWriteFast(PIN_RAS_N, HIGH);
     }
 
-    dram4164_setAddress((address >> (8 - bits)) & 0xFF);
-    digitalWriteFast(PIN_RAS_N, LOW);
-
-    for (uint16_t ix = 0; ix < blockSize; ix++)
+    uint8_t readNibble(uint16_t address)
     {
-        digitalWriteFast(PIN_WE_N, LOW);
+        setAddress((address >> 6) & 0xFF);
+        digitalWriteFast(PIN_RAS_N, LOW);
 
-        uint8_t bitValue = (value[ix >> 3] >> (ix & 0b111)) & 0b1;
+        uint8_t value = 0;
+        for (uint8_t ix = 0; ix < 4; ix++)
+        {
+            setAddress((address << 2) | ix);
+            digitalWriteFast(PIN_CAS_N, LOW);
 
-        digitalWriteFast(PIN_DI, bitValue);
+            delayMicroseconds(2);
+            value = value | ((digitalReadFast(PIN_DO) ? 1 : 0) << ix);
 
-        dram4164_setAddress((address << bits) | ix);
-        digitalWriteFast(PIN_CAS_N, LOW);
+            digitalWriteFast(PIN_CAS_N, HIGH);
+        }
 
-        digitalWriteFast(PIN_WE_N, HIGH);
-        digitalWriteFast(PIN_CAS_N, HIGH);
+        digitalWriteFast(PIN_RAS_N, HIGH);
+
+        return value;
     }
 
-    digitalWriteFast(PIN_RAS_N, HIGH);
-}
-
-void dram4164_readBlock(uint16_t blockSize, uint16_t address, uint8_t *value)
-{
-    uint8_t bits = 0;
-    uint16_t tmpBlockSize = blockSize;
-    while (tmpBlockSize = tmpBlockSize >> 1)
+    void writeBlock(uint16_t blockSize, uint16_t address, uint8_t *value)
     {
-        bits++;
+        uint8_t bits = 0;
+        uint16_t tmpBlockSize = blockSize;
+        while (tmpBlockSize = tmpBlockSize >> 1)
+        {
+            bits++;
+        }
+
+        setAddress((address >> (8 - bits)) & 0xFF);
+        digitalWriteFast(PIN_RAS_N, LOW);
+
+        for (uint16_t ix = 0; ix < blockSize; ix++)
+        {
+            digitalWriteFast(PIN_WE_N, LOW);
+
+            uint8_t bitValue = (value[ix >> 3] >> (ix & 0b111)) & 0b1;
+
+            digitalWriteFast(PIN_DI, bitValue);
+
+            setAddress((address << bits) | ix);
+            digitalWriteFast(PIN_CAS_N, LOW);
+
+            digitalWriteFast(PIN_WE_N, HIGH);
+            digitalWriteFast(PIN_CAS_N, HIGH);
+        }
+
+        digitalWriteFast(PIN_RAS_N, HIGH);
     }
 
-    memset(value, 0, (blockSize >> 3));
-
-    dram4164_setAddress((address >> (8 - bits)) & 0xFF);
-    digitalWriteFast(PIN_RAS_N, LOW);
-
-    for (uint16_t ix = 0; ix < blockSize; ix++)
+    void readBlock(uint16_t blockSize, uint16_t address, uint8_t *value)
     {
-        dram4164_setAddress((address << bits) | ix);
-        digitalWriteFast(PIN_CAS_N, LOW);
+        uint8_t bits = 0;
+        uint16_t tmpBlockSize = blockSize;
+        while (tmpBlockSize = tmpBlockSize >> 1)
+        {
+            bits++;
+        }
 
-        delayMicroseconds(2);
-        value[ix >> 3] = value[ix >> 3] | ((digitalReadFast(PIN_DO) ? 1 : 0) << (ix & 0b111));
+        memset(value, 0, (blockSize >> 3));
 
-        digitalWriteFast(PIN_CAS_N, HIGH);
+        setAddress((address >> (8 - bits)) & 0xFF);
+        digitalWriteFast(PIN_RAS_N, LOW);
+
+        for (uint16_t ix = 0; ix < blockSize; ix++)
+        {
+            setAddress((address << bits) | ix);
+            digitalWriteFast(PIN_CAS_N, LOW);
+
+            delayMicroseconds(2);
+            value[ix >> 3] = value[ix >> 3] | ((digitalReadFast(PIN_DO) ? 1 : 0) << (ix & 0b111));
+
+            digitalWriteFast(PIN_CAS_N, HIGH);
+        }
+
+        digitalWriteFast(PIN_RAS_N, HIGH);
     }
-
-    digitalWriteFast(PIN_RAS_N, HIGH);
 }
