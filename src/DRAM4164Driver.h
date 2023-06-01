5c7014319aedacd0569edd6001320418cac10ac9
Nicola Cimmino <nicola.cimmino@gmail.com>
2023-06-01 17:29:41 +0200

    Wrap in namespace

--- src/DRAM4164Driver.h
+++ src/DRAM4164Driver.h
@@ -5,6 +5,23 @@
 #include <Arduino.h>
 #include <digitalWriteFast.h>
 
+#define PIN_NC1 2
+#define PIN_DI 3
+#define PIN_WE_N 4
+#define PIN_RAS_N 5
+#define PIN_A0 6
+#define PIN_A2 7
+#define PIN_A1 8
+#define PIN_VCC 9
+#define PIN_A7 10
+#define PIN_A5 11
+#define PIN_A4 12
+#define PIN_A3 13
+#define PIN_A6 A0
+#define PIN_DO A1
+#define PIN_CAS_N A2
+#define PIN_GND A3
+
 namespace dram4164 {
     void setAddress(uint8_t address);
 
